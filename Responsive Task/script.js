df210bdc05d15568e62a9b13c9630668a9d06a15
ManasPancholi8802 <manaspancholi@gmail.com>
2026-05-28 02:42:51 +0530

    Minor Improvements in Responsive Task

--- Responsive Task/script.js
+++ Responsive Task/script.js
@@ -1,41 +1,9 @@
-const topBar = document.getElementById("topBar");
-const navbar = document.getElementById("navbar");
+const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
-
-    if (window.scrollY > 40) {
-
-        // Hide top bar
-        topBar.classList.add(
-            "opacity-0",
-            "h-0",
-            "py-0"
-        );
-
-        // Navbar compact style
-        navbar.classList.add(
-            "shadow-md",
-            "py-3"
-        );
-
-        navbar.classList.remove("py-5");
-
-    } else {
-
-        // Show top bar
-        topBar.classList.remove(
-            "opacity-0",
-            "h-0",
-            "py-0"
-        );
-
-        // Reset navbar
-        navbar.classList.remove(
-            "shadow-md",
-            "py-3"
-        );
-
-        navbar.classList.add("py-5");
-    }
-
+  if (window.scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 });
